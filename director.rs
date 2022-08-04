38cd60fa0860c44dcc539ee2a839036366ade5bc
Project-Luxy <96788441+Project-Luxy@users.noreply.github.com>
2022-08-04 12:01:11 +0200

    Perm fix

---
+++ director.rs
@@ -0,0 +1,13 @@
+use crate::driver::Op;
+
+use std::io;
+use std::path::Path;
+
+/// per-fix 
+pub async fn remove_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let op = Op::unlink_dir(path.as_ref())?;
+    let completion = op.await;
+    completion.result?;
+
+    Ok(())
+}
